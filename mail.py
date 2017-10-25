43c8736ac7310ac0c2db7d3c9ce09675e8170b1e
4hiziri <meirvg@gmail.com>
2017-10-25 14:43:39 +0900

    Use Gmail

--- mail.py
+++ mail.py
@@ -1,23 +1,70 @@
-import sys
+from configparser import ConfigParser
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formatdate
 
-smtp_server = sys.argv[1]
-from_address = 'my-ctftime-daemon'
-to_address = sys.argv[2]
-charset = 'UTF-8'
-subject = 'Next CTF!!'
-text = sys.argv[1]
-msg = MIMEText(text.encode(charset), 'plain', charset)
 
-msg['Subject'] = Header(subject, charset)
-msg['From'] = from_address
-msg['To'] = to_address
-msg['Date'] = formatdate(localtime=True)
+class Mailer:
+    def __init__(self, addr, passcode):
+        self.smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        self.addr = addr
+        self.passcode = passcode
+        self.charset = 'UTF-8'
 
-smtp = smtplib.SMTP(smtp_server)
-smtp.sendmail(from_address, to_address, msg.as_string())
+        return
 
-smtp.close()
+    def setCharset(self, charset):
+        self.charset = charset
+
+    def getMsg(self, to_addr, from_addr, subject, text):        
+        msg = MIMEText(text.encode(self.charset),
+                       'plain',
+                       self.charset)
+        msg['Subject'] = Header(subject, self.charset)
+        msg['From'] = from_addr
+        msg['To'] = to_addr
+        msg['Date'] = formatdate(localtime=True)
+
+        return msg
+
+    def send(self, to_addr, from_addr, subject, text):
+        self.smtp.ehlo()
+        self.smtp.starttls()
+        self.smtp.ehlo()
+        self.smtp.login(self.addr, self.passcode)
+        msg = self.getMsg(to_addr, from_addr, subject, text).as_string()
+        self.smtp.sendmail(from_addr,
+                           [to_addr],
+                           msg)
+
+    def close(self):
+        self.smtp.close()
+
+
+def readConfig(filename):
+    config = ConfigParser()
+    config.read(filename)
+
+    default = {'from': 'my-ctftime-daemon',
+               'charset': 'UTF-8',
+               'subject': 'Next CTF'}
+
+    default.update(config['setting'])
+
+    return default
+
+
+def main(text, configfile):
+    conf = readConfig(configfile)
+
+    mailer = Mailer(conf['addr'], conf['pass'])
+    mailer.setCharset(conf['charset'])
+    mailer.send(conf['to'], conf['from'], conf['subject'], text)
+    mailer.close()
+
+
+if __name__ == '__main__':
+    import sys
+    
+    main(sys.argv[1], 'config.conf')
